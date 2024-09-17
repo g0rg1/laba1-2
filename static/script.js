d9806cb98106c4617158423142c9175f20f86b62
Gosha Bantukow <heorhi.bantsiukou@gmail.com>
2024-09-17 17:27:17 +0300

    adaptive css and cookies-window

---
+++ static/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded" , function()  {
+    const cookieNotice = document.getElementById('cookie-notice');
+    const acceptedCookiesButton = document.getElementById('accept-cookies');
+
+    if (!sessionStorage.getItem('cookiesAccepted')){
+        cookieNotice.style.display = 'block';
+
+    }
+
+    acceptedCookiesButton.addEventListener('click' , function (){
+        sessionStorage.setItem('cookiesAccepted' , 'true');
+        cookieNotice.style.display = 'none';
+    });
+});
